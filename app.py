12b26afe86f9719ed9d119a8154460ac87574d65
annalogie-create <62751653+annalogie-create@users.noreply.github.com>
2024-10-14 17:09:20 +0200

    fix typo

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def differenzZwischenZweiPunkten(lat1,lon1,lat2,lon2):
     phi1 = math.radians(lat1)
     phi2 = math.radians(lat2)
     delta_phi = math.radians(lat2-lat1)
-    delta_lambda = math.radians(lat2-lat1)
+    delta_lambda = math.radians(lon2-lon1)
 
     a = math.sin(delta_phi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(delta_lambda/2)**2
     c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
