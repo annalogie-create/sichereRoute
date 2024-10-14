75aa5b02ce367fbb8ccb19b0ca65bec6187ff4f9
annalogie-create <62751653+annalogie-create@users.noreply.github.com>
2024-10-14 18:27:33 +0200

    added index calculation

--- app.py
+++ app.py
@@ -2,13 +2,6 @@ import pandas as pd
 import json
 import math
 
-haltestellen = pd.read_excel("data/HVV-Haltestellen.xlsx")
-print(haltestellen)
-
-meldungen = pd.read_json("data/anliegen_extern.json")
-
-print(meldungen)
-
 def differenzZwischenZweiPunkten(lat1,lon1,lat2,lon2):
     R = 6371000
     phi1 = math.radians(lat1)
@@ -22,4 +15,28 @@ def differenzZwischenZweiPunkten(lat1,lon1,lat2,lon2):
     distance = R * c
     return distance
 
-                            
+haltestellen = pd.read_excel("data/HVV-Haltestellen.xlsx", header = 0)
+
+
+with open("data/anliegen_extern.json", 'r', encoding='utf-8') as datei:
+    meldungen = json.load(datei)
+
+    indexList = []
+
+for index, row in haltestellen.iterrows(): 
+    lat1 = row[1]
+    lon1 = row[2]
+    anzahlMeldungen= 0
+    for i in range(len(meldungen['features'])): 
+        coordinates = meldungen['features'][i]['geometry']['coordinates']
+        #print(coordinates)
+        lat2= coordinates[0]
+        #print(lat2)
+        lon2 = coordinates[1]
+        if differenzZwischenZweiPunkten(lat1,lon1,lat2,lon2) < 100:
+            anzahlMeldungen += 1
+        i += 1
+    indexList.append(anzahlMeldungen)
+    print("bin dabei, index ", index, " row: ", row)
+
+print(indexList)
