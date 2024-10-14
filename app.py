d2612bed799b6fb941e37a9d8696628f8f7d5de5
annalogie-create <62751653+annalogie-create@users.noreply.github.com>
2024-10-14 17:06:24 +0200

    read data sources
    import data sources, calculate difference function

---
+++ app.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import json
+import math
+
+haltestellen = pd.read_excel("data/HVV-Haltestellen.xlsx")
+print(haltestellen)
+
+meldungen = pd.read_json("data/anliegen_extern.json")
+
+print(meldungen)
+
+def differenzZwischenZweiPunkten(lat1,lon1,lat2,lon2):
+    R = 6371000
+    phi1 = math.radians(lat1)
+    phi2 = math.radians(lat2)
+    delta_phi = math.radians(lat2-lat1)
+    delta_lambda = math.radians(lat2-lat1)
+
+    a = math.sin(delta_phi/2)**2 + math.cos(phi1)*math.cos(phi2)*math.sin(delta_lambda/2)**2
+    c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+
+    distance = R * c
+    return distance
+
+                            
